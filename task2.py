ef1f3aa5b98e00e5016c15531b7e14dc106ae6ce
sonunegi04 <sonu04120066@gmail.com>
2025-12-14 00:46:42 +0530

    Assignment4

--- task2.py
+++ task2.py
@@ -3,13 +3,11 @@ with open("output.txt", "w") as file:
     file.write(inputText + "\n")
 print("Data successfully written to output.txt.")
 
-# Step 2: Append additional data
 additional_text = input("\nEnter additional text to append: ")
 with open("output.txt", "a") as file:
     file.write(additional_text + "\n")
 print("Data successfully appended.")
 
-# Step 3: Read and display final content
 print("\nFinal content of output.txt:")
 with open("output.txt", "r") as file:
     for line in file:
